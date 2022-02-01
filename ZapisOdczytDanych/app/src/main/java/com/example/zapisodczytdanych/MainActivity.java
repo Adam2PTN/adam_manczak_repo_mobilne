1d20acd36fdbc4c05886e80922ff68c71186199f
Adam Manczak <adas.manczak@wp.pl>
2022-02-01 15:57:10 +0100

    praca pod koniec lekcji

--- ZapisOdczytDanych/app/src/main/java/com/example/zapisodczytdanych/MainActivity.java
+++ ZapisOdczytDanych/app/src/main/java/com/example/zapisodczytdanych/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.zapisodczytdanych;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+}
+
+public class MyActivity extends Activity {
+        final Button button = (Button) findViewById(R.id.save);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                File file = new File(context.getFilesDir(), "dane.txt");
+            }
+        };
 }
